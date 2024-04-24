2db0647806bab6bddf50a844d91fc301d8f5f7eb
FernandoEmilianoFloresDeLaRiva <emilianoflores07081@gmail.com>
2024-04-24 13:37:27 -0600

    .env.template added and override controller and service

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { envs } from './config/getEnvs';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('Main');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(envs.port);
+  logger.log(`Payments server started on port ${envs.port}`);
 }
 bootstrap();
---
+++ src/payments/payments.controller.ts
@@ -0,0 +1,33 @@
+import { Controller, Get, Post } from '@nestjs/common';
+import { PaymentsService } from './payments.service';
+
+@Controller('payments')
+export class PaymentsController {
+  constructor(private readonly paymentsService: PaymentsService) {}
+
+  @Post('create-payment-sesion')
+  createPaymentSession() {
+    return 'createPaymentSession';
+  }
+
+  @Get('success')
+  success() {
+    return {
+      ok: true,
+      message: 'Payment successful',
+    };
+  }
+
+  @Get('cancel')
+  cancel() {
+    return {
+      ok: false,
+      message: 'Payment cancelled',
+    };
+  }
+
+  @Post('webhook')
+  async stripeWebHook() {
+    return 'stripe.webhook';
+  }
+}
