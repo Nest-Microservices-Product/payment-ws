4084ff6b5a69dd4aa87d53538b2b6063aed3e4b9
FernandoEmilianoFloresDeLaRiva <emilianoflores07081@gmail.com>
2025-05-26 13:50:16 -0600

    healthcheck added

---
+++ src/health-check/health-check.controller.ts
@@ -0,0 +1,10 @@
+import { Controller, Get, HttpCode } from "@nestjs/common";
+
+@Controller("/")
+export class HealthCheckController {
+    @Get()
+    @HttpCode(200)
+    healthCheck(): string {
+        return "Payment ms is running!";
+    }
+}
